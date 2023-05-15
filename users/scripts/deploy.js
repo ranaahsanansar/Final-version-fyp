eb3ed23e6d7e7dc955efaf5481336bc38909ca37
Rana Ahsan Ansar <asn.cs21@gmail.com>
2023-05-15 18:27:58 +0500

    HardHat Deployment

--- users/scripts/deploy.js
+++ users/scripts/deploy.js
@@ -7,20 +7,18 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Lock = await hre.ethers.getContractFactory("OwnerShip");
+  const lock = await Lock.deploy('0xf6F304847c55f0EcC3c55640FBcDe615b08fE30e' , '0x720c8D43353236Fa47fE2eb32dba3B9dbD9eE9D7' , '0xb8B7050CdaC5154E9d56680Db23286eaf536BE37' , '0x6D775f5A4008BaAEF0FdadC09dAEe96149aB301c' );
 
   await lock.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "Contract Address is : " + lock.address
   );
 }
 
@@ -29,4 +27,15 @@ async function main() {
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
-});
+}); 
+
+
+
+// Goverment Property = 0x4ACCaDaa8AbFB3061A4a5844f8307d42D97FfF9c Verified 
+// High Court = 0x720c8D43353236Fa47fE2eb32dba3B9dbD9eE9D7 
+// LandInspector = 0xb8B7050CdaC5154E9d56680Db23286eaf536BE37 
+
+// Citizens = 0x6D775f5A4008BaAEF0FdadC09dAEe96149aB301c 
+
+
+// OnwerShip = 0xcD8D44a2c5013E3BF4Ae8Aa6CEdEe15b049EaF5A 

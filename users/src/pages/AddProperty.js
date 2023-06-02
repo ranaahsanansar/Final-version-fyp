00fa572b034e4d897b87baf4111a3fdf7b8dbcf1
Rana Ahsan Ansar <asn.cs21@gmail.com>
2023-06-02 21:06:55 +0500

    Add Validations

--- users/src/pages/AddProperty.js
+++ users/src/pages/AddProperty.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import {
   TextField,
   FormControlLabel,
@@ -11,58 +13,60 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
-  Input,
 } from "@mui/material";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
 
-const AddProperty = () => {
-  
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required("Ad Title is required"),
+  price: Yup.number().required("Price Per Share is required"),
+  priceDes: Yup.string().required("Price Description is required"),
+  headLocation: Yup.string().required("Exact Location is required"),
+  detailsLocation: Yup.string().required("Nearest Land Mark is required"),
+  city: Yup.string().required("City is required"),
+  propertyType: Yup.string().required("Property Type is required"),
+  description: Yup.string().required("Property Details is required"),
+});
 
-  const [propertyType, setPropertyType] = React.useState("house");
+const AddProperty = () => {
+  const [propertyType, setPropertyType] = useState("house");
+  const [alert, setAlert] = useState({
+    status: false,
+    msg: "",
+    type: "",
+  });
 
   const handleChange = (event) => {
     setPropertyType(event.target.value);
   };
 
-  const [alert, setAlert] = useState({
-    status: false,
-    msg: "",
-    type: ""
+  const formik = useFormik({
+    initialValues: {
+      propertyId: "",
+      title: "",
+      price: "",
+      priceDes: "",
+      headLocation: "",
+      detailsLocation: "",
+      city: "",
+      propertyType: "",
+      description: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      let confirm = window.confirm("Are you sure you want to Submit?");
+      if (confirm) {
+        setAlert({
+          status: true,
+          msg: "Submitted Successfully!",
+          type: "success",
+        });
+      }
+    },
   });
 
-  useEffect(()=>{
-    if(alert.status === true){
-        setTimeout(() => {
-      
-      setAlert({
-        status: false,
-        msg: "",
-        type: ""
-      })
-    }, 5000);
-    } 
-    
-  })
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let confirm = window.confirm("Are you sure want to Submit?");
-    if (confirm){
-      setAlert({
-      status: true,
-      msg: "Submitted Successfuly!",
-      type: "success"
-    });
-    }
-  };
   return (
-    <Box sx={{ backgroundColor: 'white' , padding: 2 , borderRadius: 2 }} >
-      <Box
-        component="form"
-        noValidate
-        sx={{ mt: 1 }}
-        id="registration-form"
-        onSubmit={handleSubmit}
-      >
+    <Box sx={{ backgroundColor: "white", padding: 2, borderRadius: 2 }}>
+      <form noValidate onSubmit={formik.handleSubmit}>
         <Grid container spacing={1}>
           <Grid item sm={6} md={6} lg={4} xs={12}>
             <TextField
@@ -71,6 +75,8 @@ const AddProperty = () => {
               id="propertyId"
               name="propertyId"
               label="Property ID"
+              value={formik.values.propertyId}
+              onChange={formik.handleChange}
             />
           </Grid>
 
@@ -82,6 +88,10 @@ const AddProperty = () => {
               id="title"
               name="title"
               label="Ad Title"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
             />
           </Grid>
 
@@ -93,6 +103,10 @@ const AddProperty = () => {
               id="price"
               name="price"
               label="Price Per Share"
+              value={formik.values.price}
+              onChange={formik.handleChange}
+              error={formik.touched.price && Boolean(formik.errors.price)}
+              helperText={formik.touched.price && formik.errors.price}
             />
           </Grid>
 
@@ -104,6 +118,10 @@ const AddProperty = () => {
               id="priceDes"
               name="priceDes"
               label="Price Description"
+              value={formik.values.priceDes}
+              onChange={formik.handleChange}
+              error={formik.touched.priceDes && Boolean(formik.errors.priceDes)}
+              helperText={formik.touched.priceDes && formik.errors.priceDes}
             />
           </Grid>
 
@@ -115,6 +133,10 @@ const AddProperty = () => {
               id="headLocation"
               name="headLocation"
               label="Exact Location"
+              value={formik.values.headLocation}
+              onChange={formik.handleChange}
+              error={formik.touched.headLocation && Boolean(formik.errors.headLocation)}
+              helperText={formik.touched.headLocation && formik.errors.headLocation}
             />
           </Grid>
 
@@ -126,27 +148,34 @@ const AddProperty = () => {
               id="detailsLocation"
               name="detailsLocation"
               label="Nearest Land Mark"
+              value={formik.values.detailsLocation}
+              onChange={formik.handleChange}
+              error={formik.touched.detailsLocation && Boolean(formik.errors.detailsLocation)}
+              helperText={formik.touched.detailsLocation && formik.errors.detailsLocation}
             />
           </Grid>
 
           <Grid item sm={6} md={6} lg={4} xs={12}>
             <FormControl fullWidth>
               <InputLabel id="city-label">City</InputLabel>
-           
-            <Select
-              fullWidth
-              required
-              labelId="city-label"
-              id="city"
-              name="city"
-              value={propertyType}
-              label="City"
-              onChange={handleChange}
-            >
-              <MenuItem value="house">Lahore</MenuItem>
-              <MenuItem value="appartment">Karachi</MenuItem>
-              <MenuItem value="plot">Quetta</MenuItem>
-            </Select>
+              <Select
+                fullWidth
+                required
+                labelId="city-label"
+                id="city"
+                name="city"
+                value={formik.values.city}
+                label="City"
+                onChange={formik.handleChange}
+                error={formik.touched.city && Boolean(formik.errors.city)}
+              >
+                <MenuItem value="house">Lahore</MenuItem>
+                <MenuItem value="appartment">Karachi</MenuItem>
+                <MenuItem value="plot">Quetta</MenuItem>
+              </Select>
+              {formik.touched.city && formik.errors.city && (
+                <Box sx={{ color: "red" }}>{formik.errors.city}</Box>
+              )}
             </FormControl>
           </Grid>
 
@@ -158,27 +187,21 @@ const AddProperty = () => {
                 labelId="property-type-label"
                 id="propertyType"
                 name="propertyType"
-                value={propertyType}
+                value={formik.values.propertyType}
                 label="Property Type"
-                onChange={handleChange}
+                onChange={formik.handleChange}
+                error={formik.touched.propertyType && Boolean(formik.errors.propertyType)}
               >
                 <MenuItem value="house">House</MenuItem>
                 <MenuItem value="appartment">Appartment</MenuItem>
-                <MenuItem value="plot">Plor</MenuItem>
+                <MenuItem value="plot">Plot</MenuItem>
               </Select>
+              {formik.touched.propertyType && formik.errors.propertyType && (
+                <Box sx={{ color: "red" }}>{formik.errors.propertyType}</Box>
+              )}
             </FormControl>
           </Grid>
 
-          {/* <Grid item sm={6} md={6} lg={4} xs={12}>
-            <TextField
-              margin="normal"
-              fullWidth
-              required
-              id="hereType"
-              name="hereType"
-              label="Title"
-            />
-          </Grid> */}
           <Grid item sm={12} md={12} lg={12} xs={12}>
             <Box>
               <h3>Property Details</h3>
@@ -189,26 +212,34 @@ const AddProperty = () => {
               placeholder="Enter details of Property"
               style={{ width: "100%", padding: "10px" }}
               name="description"
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              error={formik.touched.description && Boolean(formik.errors.description)}
             />
+            {formik.touched.description && formik.errors.description && (
+              <Box sx={{ color: "red" }}>{formik.errors.description}</Box>
+            )}
           </Grid>
 
           <Grid item sm={12} md={12} lg={12} xs={12}>
             <p>Images</p>
-            <input type='file' name="propertyImage" multiple />
-          </Grid> 
+            <input type="file" name="propertyImage" multiple />
+          </Grid>
         </Grid>
 
         <Box textAlign="center">
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{ mt: 3, mb: 2, px: 5 }}
-          >
+          <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2, px: 5 }}>
             Submit
           </Button>
         </Box>
-        {alert.status ? <Alert severity={alert.type} sx={{ mt: 3 }}>{alert.msg}</Alert> : ''}
-      </Box>
+      </form>
+      {alert.status ? (
+        <Alert severity={alert.type} sx={{ mt: 3 }}>
+          {alert.msg}
+        </Alert>
+      ) : (
+        ""
+      )}
     </Box>
   );
 };
